0b89ca02159998595215ce486d4f3036feed1789
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-04-24 18:32:39 +0200

    recursion as loop

--- recursion.exs
+++ recursion.exs
@@ -6,6 +6,19 @@ defmodule Recursion do
   def run do
     IO.puts "Factorial of 7 is #{factorial 7}"
     IO.puts "12th element of Fibonacci sequence is #{fibonacci 12}"
+
+    # recursion used as loop
+    IO.puts "Sum of list: #{sum [1, 7, 3, 9]}"
+    counter(10, 1)
+  end
+
+  def counter(val, min) do
+    if val < min do
+      nil
+    else
+      IO.puts val
+      counter(val - 1, min)
+    end
   end
 
   def factorial(val) do
@@ -23,4 +36,7 @@ defmodule Recursion do
       true -> fibonacci(n - 1) + fibonacci(n - 2)
     end
   end
+
+  def sum([]), do: 0
+  def sum([h | t]), do: h + sum(t)
 end
